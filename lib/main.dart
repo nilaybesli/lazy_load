67e016ca148d2110b6c6bad7668d86edafc44f9e
Nilay Beşli <64665975+nilaybesli@users.noreply.github.com>
2024-08-02 19:27:38 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Photo List',
+      debugShowCheckedModeBanner: false,
       initialBinding: BindingsBuilder(() {
         Get.put(PhotoController());
       }),
